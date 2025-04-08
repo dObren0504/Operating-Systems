029b5cfb2be76e7d56bfa33568b3ce0971f4caf8
dobren <andrei.dobren@student.upt.ro>
2025-04-08 15:11:34 +0300

    First commit

---
+++ treasure_manager.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+
+
+
+
+typedef struct treasure
+{
+    int treasureID;
+    char userName[30];
+    float latitude;
+    float longitude;
+    char clue[250];
+    int value;
+}treasure_t;
+
+
+
+
+
+int main (void)
+{
+    printf ("Ceau");
+    return 0;
+}
